a54c131f6d68448f11146d473eabb7fd8c6593bc
kkaris <karis.klas@gmail.com>
2023-10-10 15:52:50 -0700

    Add lambda for read_indra_db

---
+++ indra_reading/lambda_functions/read_indra_db.py
@@ -0,0 +1,52 @@
+import os
+import boto3
+from datetime import datetime
+from time import sleep
+
+BATCH = boto3.client("batch")
+
+QUEUE_NAME = "run_db_lite_queue"
+JOB_NAME = "monitor_db_reading_%s" % datetime.now().strftime("%Y%m%d")
+JOB_DEF = "run_db_lite_jobdef"
+PROJECT = "aske"
+PURPOSE = "monitor_reading"
+
+
+def lambda_handler(event, context):
+    batch = boto3.client("batch")
+    print("Getting job status from previous run...")
+    response = batch.list_jobs(
+        jobQueue=QUEUE_NAME, filters=[{"name": "JOB_NAME", "values": ["monitor*"]}]
+    )
+    if response["jobSummaryList"]:
+        # Most recent job is first in list
+        status = response["jobSummaryList"][0]["status"]
+        if status not in ["SUCCEEDED", "FAILED"]:
+            print("Previous job is still running, exiting...")
+            return "DONE"
+
+    print("Previous job is done, starting new job...")
+    core_command = f"indra-db reading run new --project-name {PROJECT}"
+    ret = BATCH.submit_job(
+        jobName=JOB_NAME,
+        jobQueue=QUEUE_NAME,
+        jobDefinition=JOB_DEF,
+        containerOverrides={
+            "command": [
+                "python3",
+                "-m",
+                "indra.util.aws",
+                "run_in_batch",
+                core_command,
+                "--project",
+                PROJECT,
+                "--purpose",
+                PURPOSE,
+            ],
+        },
+    )
+    jobId = ret["jobId"]
+
+    print("Result from job submission:\n%s" % jobId)
+
+    return "DONE"
